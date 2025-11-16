04c5e7fef0e8dfe2088a035ff97f8038936622a6
MZH006 <hashmi212411@gmail.com>
2025-11-16 17:34:10 -0500

    grid

--- project-starter-files/columns.asm
+++ project-starter-files/columns.asm
@@ -8,8 +8,8 @@
 # creation, and will indicate otherwise when it is not.
 #
 ######################## Bitmap Display Configuration ########################
-# - Unit width in pixels:       1
-# - Unit height in pixels:      1
+# - Unit width in pixels:       8
+# - Unit height in pixels:      8
 # - Display width in pixels:    256
 # - Display height in pixels:   256
 # - Base Address for Display:   0x10008000 ($gp)
@@ -18,6 +18,9 @@
     .data
 ##############################################################################
 # Immutable Data
+
+gray: .word 0x707070
+
 ##############################################################################
 # The address of the bitmap display. Don't forget to connect it!
 ADDR_DSPL:
@@ -38,7 +41,84 @@ ADDR_KBRD:
 
     # Run the game.
 main:
-    # Initialize the game
+    jal clear_screen
+    jal draw_walls
+    
+    li $v0, 10
+    syscall
+
+clear_screen:
+li $t0, 0x10008000
+li $t1, 0
+li $t2, 1024
+clear_loop:
+    sw $t1, 0($t0)
+    addi $t0, $t0, 4
+    addi $t2, $t2, -1
+    bnez $t2, clear_loop
+    jr $ra
+
+#a0 is the row
+# a1 is the column
+# the result gives the memory address of the pixel u want to draw at
+get_address:
+    li $v0, 0x10008000
+    sll $t0, $a0, 5
+    add $t0, $t0, $a1
+    sll $t0, $t0, 2
+    add $v0, $v0, $t0
+    jr $ra
+
+draw_walls:
+    addi $sp, $sp, -12
+    sw $ra, 0($sp)
+    sw $s0, 4($sp)
+    sw $s1, 8($sp)
+    lw $s0, gray
+        
+    li $a0, 0
+    li $s1, 0
+    top_loop:
+        move $a1, $s1
+        jal get_address
+        sw $s0, 0($v0)
+        addi $s1, $s1, 1
+        ble $s1, 7, top_loop
+        
+        li $a0, 25
+        li $s1, 0
+    bottom_loop:
+        move $a1, $s1
+        jal get_address
+        sw $s0, 0($v0)
+        addi $s1, $s1, 1
+        ble $s1, 7, bottom_loop
+        
+    li $a1, 0
+    li $s1, 0
+    left_loop:
+        move $a0, $s1
+        jal get_address
+        sw $s0, 0($v0)
+        addi $s1, $s1, 1
+        ble $s1, 24, left_loop
+        
+    li $a1, 7
+    li $s1, 0
+    right_loop:
+        move $a0, $s1
+        jal get_address
+        sw $s0, 0($v0)
+        addi $s1, $s1, 1
+        ble $s1, 24, right_loop
+        
+    lw $ra, 0($sp)
+    lw $s0, 4($sp)
+    lw $s1, 8($sp)
+    addi $sp, $sp, 12
+    
+    jr $ra
+
 
 game_loop:
     # 1a. Check if key has been pressed
